00067d4bae64778e41f193963faf53e926e1204b
ajea <smithajea@rocketmail.com>
2025-05-14 14:13:52 -0400

    setup project

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,13 +4,19 @@ import { defineConfig, fontProviders } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	experimental: {
-		fonts: [
-			{
-				provider: fontProviders.google(),
-				name: "Martian Mono",
-				cssVariable: "--font-martian-mono",
-				weights: [400, 600, 700],
-			},
-		],
+		// fonts: [
+		// 	{
+		// 		provider: fontProviders.google(),
+		// 		name: "Martian Mono",
+		// 		cssVariable: "--font-martian-mono",
+		// 		weights: [400, 600, 700],
+		// 	},
+		// 	{
+		// 		provider: fontProviders.google(),
+		// 		name: "Inter",
+		// 		cssVariable: "--font-martian-mono",
+		// 		weights: [400, 600, 700],
+		// 	},
+		// ],
 	},
 });
